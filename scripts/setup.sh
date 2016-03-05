dc18003497320e2878f924ef3ea2465bd9c378db
levonk <git@lk.oib.com>
2016-03-05 11:32:07 -0800

    script cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # update the system
+sudo apt-get remove -y --purge libreoffice*
 sudo apt-get update
 sudo apt-get upgrade -y
 
@@ -9,13 +10,6 @@ sudo apt-get upgrade -y
 # see https://github.com/jhipster/jhipster-docker/
 ################################################################################
 
-export JAVA_VERSION='8'
-export JAVA_HOME='/usr/lib/jvm/java-8-oracle'
-
-export MAVEN_VERSION='3.3.9'
-export MAVEN_HOME='/usr/share/maven'
-export PATH=$PATH:$MAVEN_HOME/bin
-
 export LANGUAGE='en_US.UTF-8'
 export LANG='en_US.UTF-8'
 export LC_ALL='en_US.UTF-8'
@@ -23,8 +17,6 @@ sudo locale-gen en_US.UTF-8
 sudo dpkg-reconfigure locales
 
 # we need to update to assure the latest version of the utilities
-sudo apt-get update
-
 sudo apt-get install -y git-core
 sudo apt-get install --nodeps -y etckeeper
 
@@ -35,26 +27,27 @@ sudo apt-get install -y vim git sudo zip bzip2 fontconfig curl
 sudo echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
 sudo echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
-
 sudo apt-get update
 
+export JAVA_VERSION='8'
+export JAVA_HOME='/usr/lib/jvm/java-8-oracle'
 sudo echo oracle-java-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y --force-yes oracle-java${JAVA_VERSION}-installer
 sudo  update-java-alternatives -s java-8-oracle
 
 # install maven
+export MAVEN_VERSION='3.3.9'
+export MAVEN_HOME='/usr/share/maven'
+export PATH=$PATH:$MAVEN_HOME/bin
 sudo curl -fsSL http://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz | sudo tar xzf - -C /usr/share && sudo mv /usr/share/apache-maven-${MAVEN_VERSION} /usr/share/maven && sudo ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
 
 # install node.js
 sudo curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get install -y nodejs unzip python g++ build-essential
-
 # update npm
 sudo npm install -g npm
-
 # install yeoman grunt bower grunt gulp
 sudo npm install -g yo bower grunt-cli gulp
-
 # install JHipster
 sudo npm install -g generator-jhipster@2.26.1
 
@@ -72,7 +65,6 @@ sudo locale-gen en_US en_US.UTF-8
 
 # run GUI as non-privileged user
 sudo echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
-
 # install Ubuntu desktop and VirtualBox guest tools
 sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
 sudo apt-get install -y gnome-session-flashback
@@ -102,7 +94,6 @@ export DEBIAN_FRONTEND=noninteractive
 # install Postgres with default password as 'vagrant'
 sudo apt-get install -y postgresql postgresql-client postgresql-contrib libpq-dev
 sudo -u postgres psql -c "CREATE USER admin WITH PASSWORD 'vagrant';"
-sudo apt-get install -y
 
 # install Heroku toolbelt
 sudo wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
@@ -122,20 +113,7 @@ sudo dpkg -i atom-amd64.deb
 rm -f atom-amd64.deb
 sudo dpkg --configure -a
 
-# AWS tools
-sudo apt-get install -y ec2-api-tools ec2-ami-tools
-sudo apt-get install -y iamcli rdscli moncli ascli elasticache aws-cloudformation-cli elbcli
 
-# install other tools
-sudo apt-get install -y bash-completion byobu tmux cdargs htop lsof ltrace strace zsh tofrodos ack-grep
-sudo apt-get install -y exuberant-ctags 
-sudo apt-get install -y unattended-upgrades
-sudo apt-get install -y pssh clusterssh
-
-# jq is a json formatter
-sudo apt-get install -y jq
-
-# install csv2json
 sudo apt-get install -y golang-go
 go get github.com/jehiah/json2csv
 
@@ -163,6 +141,12 @@ sudo mkdir /home/vagrant/Desktop
 ln -s /opt/sts-bundle/sts-${STS_VERSION}/STS /home/vagrant/Desktop/STS
 sudo chown -R vagrant:vagrant /home/vagrant
 
+## Get rid of unecessary items
+# online search
+sudo apt-get remove -y unity-lens-shopping
+sudo gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
+
+
 mkdir -p ~/proj/github/DGHLJ
 mkdir -p ~/proj/github/levonk
 mkdir -p ~/proj/bitbucket/levonk
@@ -180,5 +164,6 @@ sudo service ssh restart
 
 # clean the box
 sudo apt-get clean
+sudo apt-get autoremove
 dd if=/dev/zero of=/EMPTY bs=1M > /dev/null 2>&1
 rm -f /EMPTY
