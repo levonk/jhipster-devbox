86fe7f5bd9ad5d71127318198978dc8b5c1c64bc
U-SWNA\karal004 <karal004@W7-2UA4431YT6.swna.wdpr.disney.com>
2016-03-05 14:56:27 -0800

    cleaning funcationality

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 # update the system
 sudo apt-get update
-sudo apt-get upgrade -q -y
+sudo apt-get upgrade -y -q
 
 ################################################################################
 # This is a port of the JHipster Dockerfile,
@@ -17,29 +17,17 @@ sudo dpkg-reconfigure locales
 
 # we need to update to assure the latest version of the utilities
 sudo apt-get install -y -q git-core
-sudo apt-get install --nodeps -y -q etckeeper
+sudo apt-get install -y -q etckeeper
 
 # install utilities
 sudo apt-get install -y -q vim git sudo zip bzip2 fontconfig curl
 
-# install Java 8
+# install Java 8 repos
 sudo echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
 sudo echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
-sudo apt-get update
-
-export JAVA_VERSION='8'
-export JAVA_HOME='/usr/lib/jvm/java-8-oracle'
-sudo echo oracle-java-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install -y -q --force-yes oracle-java${JAVA_VERSION}-installer
-sudo  update-java-alternatives -s java-8-oracle
-
-# install maven
-export MAVEN_VERSION='3.3.9'
-export MAVEN_HOME='/usr/share/maven'
-export PATH=$PATH:$MAVEN_HOME/bin
-sudo curl -fsSL http://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz | sudo tar xzf - -C /usr/share && sudo mv /usr/share/apache-maven-${MAVEN_VERSION} /usr/share/maven && sudo ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
-
+## sudo apt-get update redundant with node installation
+#sudo apt-get update
 # install node.js
 sudo curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get install -y -q nodejs unzip python g++ build-essential
@@ -49,6 +37,20 @@ sudo npm install -g npm
 sudo npm install -g yo bower grunt-cli gulp
 # install JHipster
 sudo npm install -g generator-jhipster@2.26.1
+
+
+# install Java 8
+export JAVA_VERSION='8'
+export JAVA_HOME='/usr/lib/jvm/java-8-oracle'
+sudo echo oracle-java-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y -q --force-yes oracle-java${JAVA_VERSION}-installer
+sudo update-java-alternatives --quiet -s java-8-oracle
+
+# install maven
+export MAVEN_VERSION='3.3.9'
+export MAVEN_HOME='/usr/share/maven'
+export PATH=$PATH:$MAVEN_HOME/bin
+sudo curl -fsSL http://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz | sudo tar xzf - -C /usr/share && sudo mv /usr/share/apache-maven-${MAVEN_VERSION} /usr/share/maven && sudo ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
 
 ################################################################################
 # Install the graphical environment
@@ -63,8 +65,8 @@ sudo locale-gen en_US en_US.UTF-8
 
 
 # install Ubuntu desktop and VirtualBox guest tools
-sudo apt-get install -y -q --no ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-sudo apt-get install -y gnome-session-flashback
+sudo apt-get install -y -q ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+sudo apt-get install -y -q gnome-session-flashback
 sudo apt-get autoremove -q -y --purge libreoffice*
 # run GUI as non-privileged user
 sudo echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
@@ -76,7 +78,7 @@ sudo echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
 # install Spring Tool Suite
 export STS_VERSION='3.7.2.RELEASE'
 
-cd /opt && wget  http://dist.springsource.com/release/STS/${STS_VERSION}/dist/e4.5/spring-tool-suite-${STS_VERSION}-e4.5.1-linux-gtk-x86_64.tar.gz
+cd /opt && wget -q http://dist.springsource.com/release/STS/${STS_VERSION}/dist/e4.5/spring-tool-suite-${STS_VERSION}-e4.5.1-linux-gtk-x86_64.tar.gz
 cd /opt && tar -zxvf spring-tool-suite-${STS_VERSION}-e4.5.1-linux-gtk-x86_64.tar.gz
 cd /opt && rm -f spring-tool-suite-${STS_VERSION}-e4.5.1-linux-gtk-x86_64.tar.gz
 sudo chown -R vagrant:vagrant /opt
@@ -89,26 +91,26 @@ sudo apt-get install -y -q chromium-browser
 export DEBIAN_FRONTEND=noninteractive
 #echo 'mysql-server mysql-server/root_password password vagrant' | sudo debconf-set-selections
 #echo 'mysql-server mysql-server/root_password_again password vagrant' | sudo debconf-set-selections
-#sudo apt-get install -y mysql-server mysql-workbench
+#sudo apt-get install -y -q mysql-server mysql-workbench
 
 # install Postgres with default password as 'vagrant'
 sudo apt-get install -y -q postgresql postgresql-client postgresql-contrib libpq-dev
 sudo -u postgres psql -c "CREATE USER admin WITH PASSWORD 'vagrant';"
 
 # install Heroku toolbelt
-sudo wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+sudo wget -q -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # install Cloud Foundry client
 cd /opt && sudo curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 sudo ln -s /opt/cf /usr/bin/cf
 cd /home/vagrant
 #install Guake
-sudo apt-get install -y guake
+sudo apt-get install -y -q guake
 sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
 
 # install Atom
 
-wget https://github.com/atom/atom/releases/download/v1.3.2/atom-amd64.deb
+wget -q https://github.com/atom/atom/releases/download/v1.3.2/atom-amd64.deb
 sudo dpkg -i atom-amd64.deb
 rm -f atom-amd64.deb
 sudo dpkg --configure -a
@@ -121,9 +123,22 @@ go get github.com/jehiah/json2csv
 sudo apt-get install -y -q python3-csvkit xmlstarlet
 sudo npm install -g xml2json-command
 
+# AWS tools
+sudo apt-get install -y -q ec2-api-tools ec2-ami-tools
+sudo apt-get install -y -q iamcli rdscli moncli ascli elasticache aws-cloudformation-cli elbcli
+
+# install other tools
+sudo apt-get install -y -q bash-completion byobu tmux cdargs htop lsof ltrace strace zsh tofrodos ack-grep
+sudo apt-get install -y -q exuberant-ctags 
+sudo apt-get install -y -q unattended-upgrades
+sudo apt-get install -y -q pssh clusterssh
+
+# jq is a json formatter
+sudo apt-get install -y -q jq
+
 # No screensaver on a VM as host will lock things down
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
-sudo apt-get remove -y gnome-screensaver
+sudo apt-get remove -y -q gnome-screensaver
 
 # jekyll blogging
 curl -L https://get.rvm.io | sudo bash -s stable --ruby=2.0.0
@@ -143,7 +158,7 @@ sudo chown -R vagrant:vagrant /home/vagrant
 
 ## Get rid of unecessary items
 # online search
-sudo apt-get remove -y unity-lens-shopping
+sudo apt-get remove -y -q unity-lens-shopping
 sudo gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
 
@@ -155,7 +170,7 @@ ssh-keygen -P 'changeme' -f '/home/vagrant/.ssh/id_ecdsa' -t ecdsa -C 'devbox au
 
 # secure the system (later)
 # http://www.howtogeek.com/121650/how-to-secure-ssh-with-google-authenticators-two-factor-authentication/
-sudo apt-get install -q -y libpam-google-authenticator
+sudo apt-get install -y -q libpam-google-authenticator
 echo 'auth required pam_google_authenticator.so' >> /etc/pam.d/sshd
 echo 'ChallengeResponseAuthentication yes' >> /etc/ssh/sshd_config
 sudo service ssh restart
@@ -163,7 +178,7 @@ sudo service ssh restart
 
 
 # clean the box
-sudo apt-get clean
-sudo apt-get autoremove
+sudo apt-get clean -q
+sudo apt-get autoremove -q
 dd if=/dev/zero of=/EMPTY bs=1M > /dev/null 2>&1
 rm -f /EMPTY
