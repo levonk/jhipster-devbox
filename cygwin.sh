66d92e4187390422578efe98a3b534e019b9a966
levonk <git@lk.oib.com>
2016-03-12 11:43:51 -0800

    adding some documentation

--- cygwin.sh
+++ cygwin.sh
@@ -1,1 +1,12 @@
+## if you try to run some vagrant commands in windowslike
+## 	% vagrant destroy
+## responds with
+##	Vagrant is attempting to interface with the UI in a way that requires
+##	a TTY. Most actions in Vagrant that require a TTY have configuration
+##	switches to disable this requirement. Please do that or run Vagrant
+##	with TTY.
+## you should run the command
+##	% source cygwin.sh
+
+
 export VAGRANT_DETECTED_OS=cygwin
