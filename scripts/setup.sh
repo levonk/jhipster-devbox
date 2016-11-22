55d201e16193aaefee826ffb6eec85dc0d181b22
LevonK <git@lk.oib.com>
2016-11-22 08:54:19 +0000

    Add docker-compose installation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -51,6 +51,8 @@ echo '##### install docker repos'
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list'
 pushd . && cd /etc && sudo etckeeper commit -m "added Docker source" ; popd
+sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 ## sudo apt-get update redundant with node installation
